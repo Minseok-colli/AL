f2a0fef1d1ec43d79121cd96e64ed4baaa9a52b3
Minseok_colli <Minseok-colli@github.com>
2023-01-03 23:47:08 +0900

    230103

---
+++ 2440.py
@@ -0,0 +1,6 @@
+#2440.py
+
+n = int(input())
+
+for i in range(1,n+1):
+    print('*'*(n-i+1))
---
+++ 2441.py
@@ -0,0 +1,6 @@
+#2441.py
+
+n = int(input())
+
+for i in range(1,n+1):
+    print(" "*(i-1)+'*'*(n-i+1))
